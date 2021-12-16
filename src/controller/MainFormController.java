f4ab58befa6ed17844ddcdc305cae4800335c168
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2021-12-16 19:28:05 +0530

    main form create

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -6,5 +6,52 @@ package controller;
  *12/16/2021
  */
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private DatePicker txtBday;
+
+    @FXML
+    private CheckBox txtMale;
+
+    @FXML
+    private CheckBox txtFemale;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtSchool;
+
+    @FXML
+    private ComboBox<?> cmbCourse;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtPname;
+
+    @FXML
+    private TextField txtPcontact;
+
+    @FXML
+    private JFXButton btnReset;
+
+    @FXML
+    private JFXButton btnSubmit;
+
+    public MainFormController() {
+
+    }
 }
