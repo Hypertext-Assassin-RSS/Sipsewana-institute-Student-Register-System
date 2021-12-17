4115d70816464791c337fd00b7bcc7f75c509299
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2021-12-17 17:50:33 +0530

    BO Package add

---
+++ src/bo/BOTypes.java
@@ -0,0 +1,10 @@
+package bo;
+
+/*
+ *@author Rajith Sanjayan
+ *Sipsewana-institute-Student-Register-System
+ *12/17/2021
+ */
+
+public enum BOTypes {
+}
---
+++ src/bo/custom/ProgramBO.java
@@ -0,0 +1,10 @@
+package bo.custom;
+
+/*
+ *@author Rajith Sanjayan
+ *Sipsewana-institute-Student-Register-System
+ *12/17/2021
+ */
+
+public interface ProgramBO {
+}
